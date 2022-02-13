c6d27021c39ee8b2b8ae5eb52cc3e9a9f4e07a55
Levan Odishvili <leoodishvili@gmail.com>
2022-02-13 21:36:30 +0400

    finish crud

--- app/api/server.py
+++ app/api/server.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+from app.core import config, tasks  
+
 from app.api.routes import router as api_router
 def get_application():
     app = FastAPI(title="Phresh", version="1.0.0")
@@ -10,6 +13,8 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+    app.add_event_handler("startup", tasks.create_start_app_handler(app))
+    app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
     app.include_router(api_router, prefix="/api")
     return app
 app = get_application()
